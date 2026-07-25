c51f900f41c110839a2f29401a5bd7247e892d87
server-imp <66805612+server-imp@users.noreply.github.com>
2026-07-25 13:12:05 +0200

    add bitset class

---
+++ src/bitset.cpp
@@ -0,0 +1,52 @@
+#include "bitset.hpp"
+
+BitSet::BitSet(const size_t size)
+    : _bits((size + 63) >> 6), _size(size) {}
+
+void BitSet::set(const size_t index)
+{
+    _bits[index >> 6] |= 1ULL << (index & 63);
+}
+
+bool BitSet::get(const size_t index) const
+{
+    return _bits[index >> 6] & (1ULL << (index & 63));
+}
+
+bool BitSet::claim(const size_t index)
+{
+    const size_t   id   = index >> 6;
+    const uint64_t mask = 1ULL << (index & 63);
+
+    uint64_t& word = _bits[id];
+
+    if (word & mask)
+        return false;
+
+    word |= mask;
+    return true;
+}
+
+void BitSet::clear(const size_t index)
+{
+    _bits[index >> 6] &= ~(1ULL << (index & 63));
+}
+
+size_t BitSet::size() const
+{
+    return _size;
+}
+
+void BitSet::reset()
+{
+    std::fill(_bits.begin(), _bits.end(), 0);
+}
+
+void BitSet::flip()
+{
+    for (auto& word : _bits)
+        word = ~word;
+
+    if (const size_t extra = _bits.size() * 64 - _size)
+        _bits.back() &= (UINT64_MAX >> extra);
+}
---
+++ src/bitset.hpp
@@ -0,0 +1,22 @@
+#ifndef FW_BITSET_HPP
+#define FW_BITSET_HPP
+
+class BitSet
+{
+private:
+    std::vector<uint64_t> _bits;
+    size_t                _size;
+
+public:
+    explicit BitSet(size_t size);
+
+    void                 set(size_t index);
+    [[nodiscard]] bool   get(size_t index) const;
+    bool                 claim(size_t index);
+    void                 clear(size_t index);
+    [[nodiscard]] size_t size() const;
+    void                 reset();
+    void                 flip();
+};
+
+#endif //FW_BITSET_HPP
