2635a75a030bb9bd55c50c7756e89c21dd6162d4
server-imp <66805612+server-imp@users.noreply.github.com>
2026-07-25 23:25:36 +0200

    add toggleable class

---
+++ src/toggleable.cpp
@@ -0,0 +1,62 @@
+#include "toggleable.hpp"
+#include "logger.hpp"
+
+Toggleable::Toggleable(std::string name)
+    : _name(std::move(name)) {}
+
+const std::string& Toggleable::name() const
+{
+    return _name;
+}
+
+bool Toggleable::enabled() const
+{
+    return _enabled;
+}
+
+bool Toggleable::enable()
+{
+    LOG_INFO("Enabling \"{}\"", _name);
+
+    if (_enabled)
+    {
+        LOG_WARN("Already enabled");
+        return true;
+    }
+
+    if (!internalEnable())
+    {
+        LOG_ERR("Failed to enable \"{}\"", _name);
+        return false;
+    }
+
+    _enabled = true;
+    LOG_INFO("Enabled \"{}\"", _name);
+    return true;
+}
+
+bool Toggleable::disable()
+{
+    LOG_INFO("Disabling \"{}\"", _name);
+
+    if (!_enabled)
+    {
+        LOG_WARN("Already disabled");
+        return true;
+    }
+
+    if (!internalDisable())
+    {
+        LOG_ERR("Failed to disable \"{}\"", _name);
+        return false;
+    }
+
+    _enabled = false;
+    LOG_INFO("Disabled \"{}\"", _name);
+    return true;
+}
+
+bool Toggleable::toggle()
+{
+    return _enabled ? disable() : enable();
+}
---
+++ src/toggleable.hpp
@@ -0,0 +1,27 @@
+#ifndef FW_TOGGLEABLE_HPP
+#define FW_TOGGLEABLE_HPP
+
+class Toggleable
+{
+private:
+    std::string _name {};
+    bool        _enabled {};
+
+protected:
+    explicit Toggleable(std::string name);
+
+    virtual bool internalEnable() = 0;
+    virtual bool internalDisable() = 0;
+
+public:
+    virtual                          ~Toggleable() = default;
+    [[nodiscard]] const std::string& name() const;
+    [[nodiscard]] bool               enabled() const;
+    bool                             enable();
+    bool                             disable();
+    bool                             toggle();
+};
+
+using PToggleable = std::shared_ptr<Toggleable>;
+
+#endif //FW_TOGGLEABLE_HPP
